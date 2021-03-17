1b7228ae48f8e4955cfdc34e5333bde73c9faacd
PengXiaoShuai <1612565136@qq.com>
2021-03-17 10:10:22 +0800

    新增声明文件

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,15 @@
-/* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+import { App } from "vue";
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
+
+declare type SFCWithInstall<T> = T & {
+  install(app: App): void;
+};
+
+import powerfulTable from "el-plus-powerful-table";
+declare const _powerfulTable: SFCWithInstall<typeof powerfulTable>;
+export default _powerfulTable;
