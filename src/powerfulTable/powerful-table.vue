10d9e28c359ff7209092b5d1d2a4d80c4a131b5b
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2021-05-06 15:01:38 +0800

    update text

--- src/powerfulTable/powerful-table.vue
+++ src/powerfulTable/powerful-table.vue
@@ -446,7 +446,7 @@ export default {
 
       if (this.currentSelect.length == 0) {
         this.$message({
-          message: '请选择要操作的商品',
+          message: '请选择要操作的数据',
           type: 'warning',
           duration: 1000
         })
