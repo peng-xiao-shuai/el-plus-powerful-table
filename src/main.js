615c4b143469945290403ee695f38edb7398ffe6
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2021-07-22 17:01:25 +0800

    使用lib 之后发行文件过大

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 
 import ElementPlus from "element-plus"
-import powerfulTable from "../lib/powerful-table.umd.min.js"
+import powerfulTable from "./powerfulTable/index.js"
 
 import "element-plus/lib/theme-chalk/index.css"
 
