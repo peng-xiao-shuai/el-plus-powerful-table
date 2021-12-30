b6d7c7618bfbec56d976946d6049e90c2dd2156b
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2021-12-30 15:12:28 +0800

    fix: New tool functions

--- src/utils/format-data.ts
+++ src/utils/format-data.ts
@@ -1,9 +1,11 @@
 /**
  * 比较指定时间是否在指定时间段内
- * @param str 长时间，形如 (2008-07-22 13:04:06)
- * @returns 
+ * @param value 目标时间 可被new Date()解析
+ * @param begin 开始时间 可被new Date()解析
+ * @param end 结束时间 可被new Date()解析
+ * @returns boolean
  */
-export function compare(value: string, begin: string, end: string) {
+export function compare(value: string, begin: string, end: string):boolean {
   const valueData = new Date(value)
   const beginData = new Date(begin)
   const endData = new Date(end)
