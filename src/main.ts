4c7a58222d9cd6d42ba85657d10a9ed79e11fe0b
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2021-12-30 17:29:15 +0800

    style

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import ElementPlus from "element-plus"
 // import powerfulTable from "../lib/powerful-table.umd.min.js"
 import powerfulTable from "./powerfulTable/index"
 // import powerfulTable from 'el-plus-powerful-table-ts'
-import en from 'element-plus/lib/locale/lang/en'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 import "element-plus/theme-chalk/src/index.scss"
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(powerfulTable, {
 	// btnSlot: 'all',
+	// locale: zhCn
 })
 app.mount("#app")
