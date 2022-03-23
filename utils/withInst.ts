f6686f582f9d35c7e1c5ab7ca15a50e5b5738bdc
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2022-03-23 23:50:36 +0800

    revert: removeutils folder

--- utils/withInst.ts
+++
@@ -1,9 +0,0 @@
-import { App, Plugin } from 'vue';
-import { SFCWithInstall } from '#/powerful-table';
-export const withInst = <T>(component: T)  => {
-  (component as T & Plugin).install = (app: App) => {
-    app.component((component as any).name, component)
-  }
-
-  return component as SFCWithInstall<T>
-}
