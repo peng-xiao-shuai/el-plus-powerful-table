eea5a7616fe904c6ef7c39582ccd11b2bd57fff5
peng-xiao-shuai <1612565136@qq.com>
2022-12-20 22:16:39 +0800

    修改：取消btn注释

--- packages/powerful-table/src/powerful-table.vue
+++ packages/powerful-table/src/powerful-table.vue
@@ -175,7 +175,7 @@
                   prop.type &&
                   [
                     'image',
-                    // 'btn',
+                    'btn',
                     'switch',
                     'input',
                     'textarea',
