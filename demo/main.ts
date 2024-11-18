b4737124f8fe24b2f933570ea14f7f4b0e21ec67
peng-xiao-shuai <1612565136@qq.com>
2024-11-18 17:07:56 +0800

    fix: github Actions error

--- demo/main.ts
+++ demo/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 // import ElementPlus from 'element-plus'
-import PowerfulTable from '../es'
 import Empty from './empty'
 // import en from 'element-plus/lib/locale/lang/en'
 // import zh from 'element-plus/lib/locale/lang/zh-cn'
 // import PowerfulTable from '../es'
 import App from './App.vue'
 import { langPackages } from './indexData'
+import PowerfulTable from '~/index'
 import 'element-plus/theme-chalk/src/index.scss'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 const app = createApp(App)
